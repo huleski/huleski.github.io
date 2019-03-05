f84d4b75f3c4ec1258d22a2977d86ade72f3cddc
Holeski <huleski@gmail.com>
2019-03-05 09:41:24 +0800

    Site updated: 2019-03-05 09:41:18

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,17 @@
+// 崩溃欺骗
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/images/favicon.png");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/images/favicon.png");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
